432b43a3006d5358a14216d2286f68399e7cce99
sandiprajguru <54673092+sandiprajguru@users.noreply.github.com>
2024-01-17 16:22:48 +0530

    Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package com.scalar.day1;
+
+public class HelloWorld {
+    HelloWorld() {
+        System.out.println();
+    }
+
+    public static void main (String args[]) {
+        System.out.println("This is my Hello World Program");
+    }
+}
